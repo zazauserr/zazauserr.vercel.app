2676b0bfaff0b9ba872876438196f201880a73b7
Kudelia Bogdan <badyambula.backrooms@gmail.com>
2025-08-10 19:02:33 +0300

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -5,29 +5,30 @@ import {
   SiSlack,
   SiTableau,
   SiWindows,
-  SiPowerBI,
+  SiPowerbi,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
+        <SiWindows />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <SiSlack />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
+        <SiTableau />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPowerbi />
       </Col>
     </Row>
   );
 }
 
 export default Toolstack;
+
